0c81452a1ebd6195bc314202b14a51b4ba33156f
trungdung1711 <trungdunglebui17112004@gmail.com>
2024-03-21 15:19:31 +0700

    add returnValueDemo.c

---
+++ returnValueDemo.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+
+
+
+void* add(void* param)
+{
+    int* value = (int*)param;
+    *value = (*value)*2;
+    return value;
+}
+
+
+
+int main()
+{
+    pthread_t tid;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    int param = 100;
+
+
+    pthread_create(&tid,&attr,&add,&param);
+
+    /*doing the main thread works, and waiting for the result from the add thread*/
+    for (int i = 0 ; i < 10 ; ++i)
+    {
+        printf("Doing\n");
+        sleep(1);
+    }
+
+    void** result;
+    pthread_join(tid,result);
+    int** resultPrime = (int**)result;
+    printf("Value after executing is %d", **resultPrime);
+    return 0;
+}
