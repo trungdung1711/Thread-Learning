3a8acccd4c329da7b3c713f22d29c95791338e68
trungdung1711 <trungdunglebui17112004@gmail.com>
2024-03-21 14:44:45 +0700

    add forloopThread.c

---
+++ forLoopThread.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include <stdio.h>
+
+
+
+void* routine1(void* param)
+{
+    for (int i = 0 ; i < 20 ; ++i)
+    {
+        printf("THREAD 1\n");
+        sleep(4);
+    }
+}
+
+
+
+void* routine2(void* param)
+{
+    for (int i = 0 ; i < 20 ; ++i)
+    {
+        printf("THREAD 2\n");
+        sleep(4);
+    }
+}
+
+
+
+int main()
+{
+    pthread_t tid[2];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    for (int i = 0 ; i < 2 ; ++i)
+    {
+        pthread_create(&tid[i],&attr,&routine1,NULL);
+        pthread_join(tid[i],NULL);
+    }
+    return 0;
+}
